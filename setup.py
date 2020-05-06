431c3252e877bca15db2ccb01c11a2c77ac106f8
Kelly <guojiyou40020@hotmail.com>
2020-05-06 13:37:12 +0800

    update

--- setup.py
+++ setup.py
@@ -1,9 +1,12 @@
 from distutils.core import setup
 
+import word_spell
+
 setup(
         name='word-spell',
         version='0.5.0',
-        py_modules=['word_spell.py'],
+        py_modules=['word_spell'],
+        scripts=['word_spell.py'],
         url='',
         license='MIT',
         author='Kelly',
